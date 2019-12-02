6702b2c1e964581dbc0da0f07d84fda2edc2b2ac
Seoksoon Jang <boraseoksoon@gmail.com>
2019-12-03 00:02:15 +0900

    [FIX] SwiftUI target build to be the first

--- Example/SwiftUIExample/ContentView.swift
+++ Example/SwiftUIExample/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                     Image(systemName: "grid")
                     Text("Grid")
             }
-        }.edgesIgnoringSafeArea(.all)
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
